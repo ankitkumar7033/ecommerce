faa582aac4785330c1079053e769f558fe90790a
ankitkumar7033 <ankit483850@gmail.com>
2024-07-12 22:28:20 +0530

    new

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 echo "BUILD START"
 apt-get update && apt-get install -y postgresql libpq-dev
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
