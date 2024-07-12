574ca10be4f4f7c625f715c8cfc74a9570d67aa4
ankitkumar7033 <ankit483850@gmail.com>
2024-07-12 22:10:20 +0530

    w

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
+# build_files.sh
 echo "BUILD START"
+apt-get update && apt-get install -y postgresql libpq-dev
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END" 
+echo "BUILD END"
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR,]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
